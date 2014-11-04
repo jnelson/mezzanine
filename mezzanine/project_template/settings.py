8c99a0dea601c05825710ddf9b260c5b503437e0
Micah Lucas <micahjlucas@gmail.com>
2014-11-03 16:01:10 -0800

    Updated BlogPost model. Upgraded DRF to v3.0

--- mezzanine/project_template/settings.py
+++ mezzanine/project_template/settings.py
@@ -385,7 +385,7 @@ else:
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.OAuth2Authentication',
