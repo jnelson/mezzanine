bccb9ef5aac643256a769f9f9e2f2d2a8504d912
Jonathan Nelson <jnelson@users.noreply.github.com>
2015-03-08 16:37:01 -0400

    checks for existing settings module

--- mezzanine/project_template/wsgi.py
+++ mezzanine/project_template/wsgi.py
@@ -3,8 +3,9 @@ from __future__ import unicode_literals
 import os
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+if not os.environ.get("DJANGO_SETTINGS_MODULE"):
+    settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
